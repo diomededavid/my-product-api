182a0b651d49e30ac89bbcd7d60c13f2f014e73a
David Diomede <david@d2creativedesign.com>
2026-04-23 19:23:14 -0400

    Product api stack v2

--- stacks/product_api_stack.py
+++ stacks/product_api_stack.py
@@ -1,11 +1,40 @@
-from aws_cdk import Stack
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
+    CfnOutput
+)
 from constructs import Construct
-
+import os
 
 class ProductApiStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Add your CDK resources here
+        # Lambda function for get_product
+        lambda_fn = _lambda.Function(
+            self, "GetProductFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="get_product.handler",
+            code=_lambda.Code.from_asset(os.path.join(os.path.dirname(__file__), '../lambda_code')),
+            environment={}
+        )
 
+        # API Gateway REST API
+        api = apigateway.LambdaRestApi(
+            self, "ProductsApi",
+            handler=lambda_fn,
+            proxy=False
+        )
+
+        # /products/{id} endpoint
+        products = api.root.add_resource("products")
+        product_id = products.add_resource("{id}")
+        product_id.add_method("GET")
+
+        # Output the API URL
+        CfnOutput(
+            self, "ProductsApiUrl",
+            value=api.url,
+            description="URL of the Products API"
+        )
